bb5953ef16dfd6374c824ef0a714e6952b587544
Dhevesh <dheveshp@gmail.com>
2019-04-09 16:44:14 +0100

    intial setup of class

--- Logger/Logger/Program.cs
+++ Logger/Logger/Program.cs
@@ -3,11 +3,54 @@ using System.IO;
 
 namespace Logger
 {
+    internal class LogToFile
+    {
+        internal void CheckDirectory(string filePath)
+        {
+            if (Directory.Exists(filePath))
+            {
+                Console.WriteLine("Directory Exists!");
+                File.Create(filePath);
+            }
+            else
+            {
+                Directory.CreateDirectory(filePath);
+                Console.WriteLine("Directory created");
+            }
+        }
+        internal void InfoLog()
+        {
+            string filePath = @"C:\TestLogs";
+            try
+            {
+                CheckDirectory(filePath);
+                string[] files = Directory.GetFiles(filePath);
+                foreach (string _files in files)
+                {
+                    if (string.IsNullOrEmpty(_files))
+                    {
+                        Console.WriteLine("No files found at the specified location");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"File found {_files}");
+                    }
+                }
+            }
+            catch (DirectoryNotFoundException)
+            {
+
+            }
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
+            LogToFile _LogToFile = new LogToFile();
+            _LogToFile.InfoLog();
+            
         }
     }
 }
