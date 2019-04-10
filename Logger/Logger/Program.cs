8be20d4ad6221fa605969745f1a80e59c4ede278
IMS\Dheveshp <dheveshp@gmail.com>
2019-04-10 11:39:20 +0100

    Info logger functionality implemented.

--- Logger/Logger/Program.cs
+++ Logger/Logger/Program.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace Logger
 {
     internal class LogToFile
     {
-        internal void CheckDirectory(string filePath)
+        readonly string filePath = @"c:\TestLogs\";
+
+        internal void CheckDirectory()
         {
             if (Directory.Exists(filePath))
             {
                 Console.WriteLine("Directory Exists!");
-                File.Create(filePath);
             }
             else
             {
@@ -18,28 +20,37 @@ namespace Logger
                 Console.WriteLine("Directory created");
             }
         }
-        internal void InfoLog()
+        //Takes in the error exception as an input
+        internal void InfoLog(string e)
         {
-            string filePath = @"C:\TestLogs";
-            try
+            string _date = $"{DateTime.Now:yyyyMMdd}";
+            string _time = $"{DateTime.Now.ToShortTimeString()} [INFO]";
+            string _file = $"{filePath}log_{_date}.txt";
+            if (!Directory.EnumerateFileSystemEntries(filePath).Any())
             {
-                CheckDirectory(filePath);
-                string[] files = Directory.GetFiles(filePath);
-                foreach (string _files in files)
+                using (var _fileStream = new StreamWriter(_file, true))
                 {
-                    if (string.IsNullOrEmpty(_files))
+                    _fileStream.WriteLine($"{_time} {e}");
+                }
+                
+            }
+            else
+            {
+                if (File.Exists(_file))
+                {
+                    using (var _fileStream = new StreamWriter(_file, true))
                     {
-                        Console.WriteLine("No files found at the specified location");
+                        _fileStream.WriteLine($"{_time} {e}");
                     }
-                    else
+                    
+                }
+                else
+                {
+                    using (var _fileStream = new StreamWriter(_file, true))
                     {
-                        Console.WriteLine($"File found {_files}");
+                        _fileStream.WriteLine($"{_time} {e}");
                     }
                 }
-            }
-            catch (DirectoryNotFoundException)
-            {
-
             }
         }
     }
@@ -49,8 +60,17 @@ namespace Logger
         static void Main(string[] args)
         {
             LogToFile _LogToFile = new LogToFile();
-            _LogToFile.InfoLog();
-            
+            _LogToFile.CheckDirectory();
+            try
+            {
+                Console.Write("Enter a number");
+                string number = Console.ReadLine();
+                int stringToInt = int.Parse(number);
+            }
+            catch (Exception e)
+            {
+                _LogToFile.InfoLog(e.ToString());
+            }
         }
     }
 }
